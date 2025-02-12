a71aa2bbd94adc10ffcf5cbb45b5989842109dd7
wakio-ngari <ngarijoyce017@gmail.com>
2025-02-12 14:06:14 +0300

    completed sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    
+    if length == 0:
+        print([])
+        return
+    
+    fibonacci_sequence = [0, 1]
+    
+    if length == 1:
+        print([0])
+        return
+    
+    while len(fibonacci_sequence)< length:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+    
+   
+    print(fibonacci_sequence[:length])
